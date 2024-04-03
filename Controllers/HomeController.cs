a6ce6de570f755d7932b2081858ed420f96df6b7
Sibongamandla Mnyandu <u21566098@up.ac.za>
2024-04-03 14:12:45 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using S1L05_RWD.Models;
+using System.Diagnostics;
+
+namespace S1L05_RWD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Consult() 
+        {
+            return View();
+        }
+
+		public IActionResult Schedule()
+		{
+			return View();
+		}
+
+        
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
